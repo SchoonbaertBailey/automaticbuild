04862ad4f773c3ba524d1dad3574305b4d009dca
Bailey Schoonbaert <bailey.schoonbaert@student.howest.be>
2025-11-18 14:20:03 +0100

    test

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://schoonbaertbailey.github.io",
+  base: "/automaticbuild",
+});
